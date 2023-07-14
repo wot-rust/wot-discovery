01687052c9de8e160df3871307e5d8fd577ee49a
Luca Barbato <lu_zero@gentoo.org>
2023-07-14 09:31:18 +0200

    fix: Unbreak test

--- tests/mdns.rs
+++ tests/mdns.rs
@@ -12,11 +12,11 @@ use wot_td::extend::ExtendableThing;
 async fn run_servient() {
     let servient = Servient::builder("TestThing")
         .finish_extend()
-        .http_bind("127.0.0.1:8080".parse().unwrap())
+        .http_bind("0.0.0.0:8080".parse().unwrap())
         .build_servient()
         .unwrap();
 
-    eprintln!("Listening to 127.0.0.1:8080");
+    eprintln!("Listening to 0.0.0.0:8080");
 
     let _ = tokio::time::timeout(Duration::from_secs(30), async {
         servient.serve().await.unwrap()
@@ -43,37 +43,42 @@ impl ExtendableThing for A {
 async fn discoverer() -> Result<(), Box<dyn std::error::Error>> {
     let local = task::LocalSet::new();
 
+    std::thread::spawn(move || {
+        let rt = tokio::runtime::Builder::new_current_thread()
+            .enable_all()
+            .build()
+            .unwrap();
+
+        rt.block_on(run_servient());
+    });
+
     local
         .run_until(async {
             task::spawn_local(async {
-                run_servient().await;
-            });
-
-            task::spawn_local(async {
                 let d = Discoverer::new().unwrap();
 
-                let t = std::pin::pin!(d.stream().unwrap())
-                    .next()
-                    .await
-                    .unwrap()
-                    .unwrap();
+                let t = std::pin::pin!(d.stream().unwrap()).next().await.unwrap()?;
 
                 assert_eq!("TestThing", t.title);
-            });
+                println!("Found {}", t.title);
+                Result::<(), Box<dyn std::error::Error>>::Ok(())
+            })
+            .await??;
 
             task::spawn_local(async {
                 let d = Discoverer::new().unwrap().ext::<A>();
 
-                let t = std::pin::pin!(d.stream().unwrap())
-                    .next()
-                    .await
-                    .unwrap()
-                    .unwrap();
+                let t = std::pin::pin!(d.stream().unwrap()).next().await.unwrap()?;
 
                 assert_eq!("TestThing", t.title);
-            });
+                println!("Found {}", t.title);
+                Result::<(), Box<dyn std::error::Error>>::Ok(())
+            })
+            .await??;
+
+            Result::<(), Box<dyn std::error::Error>>::Ok(())
         })
-        .await;
+        .await?;
 
     Ok(())
 }
